5e58ef591dfd67762f12842c44452245f930d23e
Nguyen Vu Hoa Binh <nguyenvuhoabinh.ai@gmail.com>
2025-04-21 10:11:59 +0700

    init project

---
+++ src/test/java/binhnvh/prototype_8/Prototype8ApplicationTests.java
@@ -0,0 +1,13 @@
+package binhnvh.prototype_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prototype8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
